1eb0ad16c6d7c22b0f2ed16e409b69a0d3faca3b
Shiro <noum.rzdr@gmail.com>
2025-01-30 16:44:07 +0300

    add server and metrics

---
+++ src/core/domain/metrics.py
@@ -0,0 +1,6 @@
+class Metrics:
+    def __init__(self, CPU, RAM, DISK, UPTIME):
+        self.CPU = CPU
+        self.RAM = RAM
+        self.DISK = DISK
+        self.UPTIME = UPTIME
---
+++ src/core/domain/server.py
@@ -0,0 +1,7 @@
+class Server:
+    def __init__(self, hostname, ip_address, port,user, password):
+        self.hostname = hostname
+        self.ip_address = ip_address
+        self.port = port
+        self.user = user
+        self.password = password
